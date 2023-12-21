d8afc67e77394334d9c9652b84b0095f215cb1a3
Jasand <smileonyourface@yandex.com>
2023-12-21 11:51:09 +0500

    Homework

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1. Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+int String(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int Sum(int N, int M)
+{
+  if (N == M) return N;
+  else return Sum(N + 1, M) + N;
+}
+
+int N = String("Введите первое число: ");
+int M = String("Введите второе число: ");
+Console.WriteLine($"Сумма между этими числами: {Sum(N, M)}");
---
+++ 03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3. Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void MyNumber(int n)
+{
+    if (n == 0) return;
+    {
+        MyNumber(n - 1);
+        Console.WriteLine(n);
+    }
+}
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+MyNumber(N);
